342563e27456773c8e027d64a18b766cea9cf091
pekko <pekko1658@gmail.com>
2022-05-14 12:07:17 +0800

    May 13, 2022

---
+++ May 13, 2022/E354_RussianDollEnvelopes.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class E354_RussianDollEnvelopes {
+    //依旧动态规划-------太慢
+    public int maxEnvelopes(int[][] envelopes) {
+        //先排序
+        int n = envelopes.length;
+        if (n == 0) {
+            return 0;
+        }
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] e1, int[] e2) {
+                if (e1[0] != e2[0]) {
+                    return e1[0] - e2[0];
+                } else {
+                    return e2[1] - e1[1];
+                }
+            }
+        });
+
+        int[] f = new int[envelopes.length];
+        Arrays.fill(f, 1);
+        int ans = 1;
+        for (int i = 1; i < envelopes.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            ans = Math.max (ans, f[i]);
+        }
+        return ans;
+    }
+
+
+    //二分法+ 动态规划 一定重新做 根本不会
+    public int maxEnvelopes2(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] e1, int[] e2) {
+                if (e1[0] != e2[0]) {
+                    return e1[0] - e2[0];
+                } else {
+                    return e2[1] - e1[1];
+                }
+            }
+        });
+
+        List<Integer> f = new ArrayList<Integer>();
+        f.add(envelopes[0][1]);
+        for (int i = 1; i < n; ++i) {
+            int num = envelopes[i][1];
+            if (num > f.get(f.size() - 1)) {
+                f.add(num);
+            } else {
+                int index = binarySearch(f, num);
+                f.set(index, num);
+            }
+        }
+        return f.size();
+    }
+
+    public int binarySearch(List<Integer> f, int target) {
+        int low = 0, high = f.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (f.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+
+}
