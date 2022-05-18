84fb5e5555b69f82f8fd5645a4736da3241f699b
pekko <pekko1658@gmail.com>
2022-05-19 02:44:22 +0800

    May 18, 2022

---
+++ 2. String (2022-5)/May 18, 2022/E158_ReadNCharactersGivenRead4IICallMultipleTimes.java
@@ -0,0 +1,13 @@
+public class E158_ReadNCharactersGivenRead4IICallMultipleTimes {
+
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        char[] buf4 = new char[4];
+
+    }
+
+    }
