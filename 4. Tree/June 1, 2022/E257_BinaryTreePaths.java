040ee885009d21ec781de2399c847d8d3e48a317
pekokk <zh0208@163.com>
2022-06-01 23:30:51 -0400

    June 1, 2022

---
+++ 4. Tree/June 1, 2022/E257_BinaryTreePaths.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+/**
+ * <p></p>
+ * <p></p>
+ *
+ * @author: He Zhao
+ * @create: 2022-06-01 21:19
+ */
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//BFS+QUEUE
+public class E257_BinaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        if (root == null) {
+            return paths;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<String> pathQueue = new LinkedList<String>();
+
+        nodeQueue.offer(root);
+        pathQueue.offer(Integer.toString(root.val));
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            String path = pathQueue.poll();
+
+            if (node.left == null && node.right == null) {
+                paths.add(path);
+            } else {
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.left.val).toString());
+                }
+
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.right.val).toString());
+                }
+            }
+        }
+        return paths;
+
+
+
+
+
+
+    }
+}
